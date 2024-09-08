74bce93bb058875c096d459e13622499ff07ced2
ZhantyDev <unpajarocantor07@gmail.com>
2024-09-08 13:12:50 -0500

    se hace la parte logica en js y se cuadran unas cosas del css, no se pudo agregar los nombres de los personajes aleatorios por un error mk que no lee los nombres del vector(preguntarle al profe):D)

--- procesos.js
+++ procesos.js
@@ -12,7 +12,7 @@ const valorInput = () => {
 const MostrarReco = () => {
     let vec = []
     let vecData=[]
-    for (let i = 0; i<20 ; i++){
+    for (let i = 0; i<19 ; i++){
         numAl= Math.floor(Math.random() * (826)) + 1;               
         vec.push(numAl)   
         const endPoint = `character/${numAl}`
@@ -21,12 +21,29 @@ const MostrarReco = () => {
         .then(res => vecData.push(res.data.name))
         .catch(err => console.log(err))      
     }  
-    console.log(vecData)
     console.log(vec)
+    console.log(vecData)
     let respuesta = document.getElementById('reco')
     respuesta.innerHTML=`
-    <h2 class = "text">${vec[0]} - ${vecData[0]}</h2>    
-
+    <h2 class = "text">${vec[0]}</h2>    
+    <h2 class = "text">${vec[1]}</h2>    
+    <h2 class = "text">${vec[2]}</h2>    
+    <h2 class = "text">${vec[3]}</h2>    
+    <h2 class = "text">${vec[4]}</h2>    
+    <h2 class = "text">${vec[5]}</h2>    
+    <h2 class = "text">${vec[6]}</h2>    
+    <h2 class = "text">${vec[7]}</h2>    
+    <h2 class = "text">${vec[8]}</h2>    
+    <h2 class = "text">${vec[9]}</h2>    
+    <h2 class = "text">${vec[10]}</h2>    
+    <h2 class = "text">${vec[11]}</h2>    
+    <h2 class = "text">${vec[12]}</h2>    
+    <h2 class = "text">${vec[13]}</h2>    
+    <h2 class = "text">${vec[14]}</h2>    
+    <h2 class = "text">${vec[15]}</h2>    
+    <h2 class = "text">${vec[16]}</h2>    
+    <h2 class = "text">${vec[17]}</h2>    
+    <h2 class = "text">${vec[18]}</h2>
     `
 }
 
