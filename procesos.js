95e21b6d90703cb5cd7fbe4f4d11449a85949011
ZhantyDev <unpajarocantor07@gmail.com>
2024-09-08 12:16:22 -0500

    se hace la parte logica con el consumo de la api y se cuadra unas propiedades de la clase text

---
+++ procesos.js
@@ -0,0 +1,44 @@
+const BaseUrl = "https://rickandmortyapi.com/api/"
+
+const valorInput = () => {
+    let textoImput = document.getElementById('input_juego')
+    let texto = textoImput.value   
+    const endPoint = `character/${texto}`
+    const url = `${BaseUrl}${endPoint}`     
+    axios.get(url)
+    .then(res => ImprimirInfo(res.data))
+    .catch(err => console.log(err))
+}
+const MostrarReco = () => {
+    let vec = []
+    let vecData=[]
+    for (let i = 0; i<20 ; i++){
+        numAl= Math.floor(Math.random() * (826)) + 1;               
+        vec.push(numAl)   
+        const endPoint = `character/${numAl}`
+        const url = `${BaseUrl}${endPoint}`     
+        axios.get(url)
+        .then(res => vecData.push(res.data.name))
+        .catch(err => console.log(err))      
+    }  
+    console.log(vecData)
+    console.log(vec)
+    let respuesta = document.getElementById('reco')
+    respuesta.innerHTML=`
+    <h2 class = "text">${vec[0]} - ${vecData[0]}</h2>    
+
+    `
+}
+
+const ImprimirInfo = (data) => {
+    let respuesta = document.getElementById('impresion')
+    respuesta.innerHTML=`
+    <h2 class = "text">Nombre: ${data.name}</h2>
+    <h2 class = "text">Origen: ${data.origin['name']}</h2>    
+    <h2 class = "text">especie: ${data.species}</h2>    
+    <h2 class = "text">genero: ${data.gender}</h2>    
+    <h2 class = "text" >estado: ${data.status}</h2>
+    <img src ="${data.image}"></img>
+    `
+}
+window.onload = MostrarReco
